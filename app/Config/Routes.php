59925bf63dd346b99676ece2282f8dc56fb9995d
eddyh99 <eddy_h99@yahoo.com>
2025-06-06 20:07:56 +0800

    Change Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->get('price', 'V1\Price::getIndex');
 $routes->get('/', 'Home::index');
 $routes->group('non', static function ($routes) {
     $routes->post('notify_payment', 'Payment::postUpdate_status');
