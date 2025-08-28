9495b9aa88a887e8958b38c938b8395fd109aaf8
yanevannn <yanevan03@gmail.com>
2025-08-28 16:48:02 +0800

    Update: refine referral ID assignment logic in postChange_upline method

--- app/Controllers/V1/Member.php
+++ app/Controllers/V1/Member.php
@@ -493,7 +493,7 @@ class Member extends BaseController
         }
 
         $newReferralId = null;
-        if (!($referral['email'] === 'a@a.a' || $referral['refcode'] === 'm4573r')) {
+        if ($referral['refcode'] != 'm4573r') {
             $newReferralId = $referral['id'];
         }
 
